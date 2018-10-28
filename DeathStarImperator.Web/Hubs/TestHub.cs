0415eb5f15dbde991b201d253a60f33745d14a57
Sean 'Doc' Rinehart <sean.rinehart@headspring.com>
2018-10-28 22:26:57 +0800

    Add initial setup and some sample code for SignalR in react-redux

---
+++ DeathStarImperator.Web/Hubs/TestHub.cs
@@ -0,0 +1,13 @@
+﻿namespace DeathStarImperator.Web.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+    using System.Threading.Tasks;
+
+    public class TestHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
